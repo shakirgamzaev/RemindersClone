23c4de12177576034f089fc74f404cf849bb0124
Shakir Gamzaev <shkir100@gmail.com>
2024-10-15 19:26:33 +0200

    Initial Commit

---
+++ RemindersClone/RemindersCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersCloneApp.swift
+//  RemindersClone
+//
+//  Created by shakir Gamzaev on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
